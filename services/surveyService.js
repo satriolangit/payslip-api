28cfd58869efd9a7657d6d3285abc5058eac7401
Wahyu Wadiantoro <wahyu.wadiantoro@sinergymediaintegrasi.com>
2021-09-27 07:02:53 +0700

    insert department to survey

--- services/surveyService.js
+++ services/surveyService.js
@@ -1,17 +1,14 @@
 const db = require('../config/database');
 const uuidv4 = require('uuid/v4');
 const moment = require('moment');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config');
 
-const createSurvey = async (submittedBy, reason, result) => {
+const createSurvey = async (submittedBy, reason, result, department) => {
 	//create new user
     
 	const timestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
 	const sql =
-		'INSERT INTO survey (id, submittedBy, submittedAt, result, reason) ' +
-		'VALUES (?, ?, ?, ?, ?)';
+		'INSERT INTO survey (id, submittedBy, submittedAt, result, reason, department) ' +
+		'VALUES (?, ?, ?, ?, ?, ?)';
 
 	const id = uuidv4();
 
@@ -20,7 +17,8 @@ const createSurvey = async (submittedBy, reason, result) => {
 		submittedBy,
 		timestamp,
 		result,
-		reason
+		reason,
+		department
 	]);
 
 	return id;
@@ -40,13 +38,13 @@ const createSurveyImage = (surveyId, imageName) => {
 
 const getReport = async () => {	
 
-    const sql = `SELECT ROW_NUMBER() OVER (ORDER BY s.id) AS no,
-        MONTHNAME(s.submittedAt) AS bulan, YEAR(s.submittedAt) AS tahun, DATE_FORMAT(s.submittedAt, '%d/%m/%Y') AS submittedAt,
-        s.submittedBy AS nik, usr.name AS nama, '-' AS department, s.result, s.reason,
-        (SELECT GROUP_CONCAT(DISTINCT imageName SEPARATOR ';') FROM survey_images WHERE surveyId = s.id) AS photos
-        FROM survey s INNER JOIN user usr ON usr.employee_id = s.submittedBy`;
+    // const sql = `SELECT ROW_NUMBER() OVER (ORDER BY s.id) AS no,
+    //     MONTHNAME(s.submittedAt) AS bulan, YEAR(s.submittedAt) AS tahun, DATE_FORMAT(s.submittedAt, '%d/%m/%Y') AS submittedAt,
+    //     s.submittedBy AS nik, usr.name AS nama, '-' AS department, s.result, s.reason,
+    //     (SELECT GROUP_CONCAT(DISTINCT imageName SEPARATOR ';') FROM survey_images WHERE surveyId = s.id) AS photos
+    //     FROM survey s INNER JOIN user usr ON usr.employee_id = s.submittedBy`;
 
-	sql = `SELECT MONTHNAME(s.submittedAt) AS bulan, YEAR(s.submittedAt) AS tahun, DATE_FORMAT(s.submittedAt, '%d/%m/%Y') AS submittedAt,s.submittedBy AS nik, usr.name AS nama, '-' AS department, s.result, s.reason,(SELECT GROUP_CONCAT(DISTINCT imageName SEPARATOR ';') FROM survey_images WHERE surveyId = s.id) AS photos FROM survey s INNER JOIN user usr ON usr.employee_id = s.submittedBy ORDER BY s.SubmittedAt`;
+	const sql = `SELECT MONTHNAME(s.submittedAt) AS bulan, YEAR(s.submittedAt) AS tahun, DATE_FORMAT(s.submittedAt, '%d/%m/%Y') AS submittedAt,s.submittedBy AS nik, usr.name AS nama, s.department, s.result, s.reason,(SELECT GROUP_CONCAT(DISTINCT imageName SEPARATOR ',') FROM survey_images WHERE surveyId = s.id) AS photos FROM survey s INNER JOIN user usr ON usr.employee_id = s.submittedBy ORDER BY s.SubmittedAt`;
 
 
 	// sql = `SET @row_number = 0;
